6c0d8f06871a8cfc82fbb149262f1949d21fd2e9
hoganord <alexwood@manchester.ac.uk>
2012-03-07 13:29:54 +0000

    Precompile Skeleton CSS in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ Sample1App::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  # Precompile Blueprint CSS
+    config.assets.precompile += [ "skeleton/*.css" ]
 end
