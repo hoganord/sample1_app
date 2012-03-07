afe32a251a300f95abf0ef76041a157ffb026f8f
hoganord <alexwood@manchester.ac.uk>
2012-03-07 13:03:47 +0000

    Remove default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Sample1App::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
+  	match '/help',    to: 'static_pages#help'
+    match '/about',   to: 'static_pages#about'
+    match '/contact', to: 'static_pages#contact'
   
-  get "static_pages/about"
+    root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
